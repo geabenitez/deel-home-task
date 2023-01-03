703e9a0b6e61f1ccbff2fac2eb0ea38850c58718
Gerson E. Aguirre <hola@gersonaguirre.me>
2023-01-03 11:34:34 -0600

    IMPLEMENTS JOBS/UNPAID ENDPOINT Adds the GET /jobs/unpaid endpoint that returs all unpaid jobs for a profile id for active contracts

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @returns all unpaid jobs for a profile id for active contracts
+ */
+router.get('/unpaid', async (req, res) => {
+  const { Contract, Job } = req.app.get('models')
+
+  // Get all contract ids for a profile id and status in_progress
+  const contractIds = (await Contract.findAll({
+    where: {
+      ClientId: req.profile.id,
+      status: 'in_progress'
+    }
+  })).map(contract => contract.id)
+
+  // Get all jobs for contract ids and paid is null (unpaid)
+  const jobs = await Job.findAll({
+    where: {
+      ContractId: contractIds,
+      paid: null
+    }
+  })
+  res.json(jobs)
+})
 
 module.exports = router;
